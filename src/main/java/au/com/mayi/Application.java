21fd9106168521e8fff77f78e124711ba64678f6
Dheeraj Agrawal <dheeraj.agrawal@industrieit.com>
2019-09-30 13:35:55 +1000

    Adding the docker compose and refactoring a bit

--- src/main/java/au/com/mayi/Application.java
+++ src/main/java/au/com/mayi/Application.java
@@ -1,5 +1,7 @@
 package au.com.mayi;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +9,18 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableKafka
 @SpringBootApplication
-public class Application {
+public class Application implements InitializingBean {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	// @Value("${/parameter_name}")
+	// String value;
 
 	@Configuration
 	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
@@ -20,5 +28,11 @@ public class Application {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
 		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// log.info("From parameter store: {}", value);
+
 	}
 }
